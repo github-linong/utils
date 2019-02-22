f7abe21a65a0c0fbafacb63abcde76d2cb017ff2
linong <linong@mail.51vv.com>
2019-02-22 17:13:25 +0800

    增加文档

--- src/index.js
+++ src/index.js
@@ -1,94 +1,94 @@
 let ua = navigator.userAgent;
 let utils = {
-	// 移动端
-	isMobile: ("ontouchstart" in window || navigator.msMaxTouchPoints) ? true : false,
-    // 微信
-    isWechat: /micromessenger/gi.test(ua),
-    // qq
-    isQQ: /qq/gi.test(ua),
-    // VV音乐
-    isvvmusic: /vvmusic/gi.test(ua),
-    // 安卓
-    isAndroid: /android/gi.test(ua),
-    // iOS
-    isIOS: /iphone|ipad|ipod|itouch/gi.test(ua); // IOS
-    
-    // JSONP请求数据
-	JSONP: (function(){
-        var count = 0;
-        function noop(){}
-        /**
-         * JSONP handler
-         *
-         * Options:
-         *  - param {String} qs parameter (`callback`)
-         *  - prefix {String} qs parameter (`__jp`)
-         *  - name {String} qs parameter (`prefix` + incr)
-         *  - timeout {Number} how long after a timeout error is emitted (`60000`)
-         *
-         * @param {String} url
-         * @param {Object|Function} optional options / callback
-         * @param {Function} optional callback
-         */
-        function jsonp(url, opts, fn){
-           if ('function' == typeof opts) {
-             fn = opts;
-             opts = {};
-           }
-           if (!opts) opts = {};
+  // 移动端
+  isMobile: ("ontouchstart" in window || navigator.msMaxTouchPoints) ? true : false,
+  // 微信
+  isWechat: /micromessenger/gi.test(ua),
+  // qq
+  isQQ: /qq/gi.test(ua),
+  // VV音乐
+  isvvmusic: /vvmusic/gi.test(ua),
+  // 安卓
+  isAndroid: /android/gi.test(ua),
+  // iOS
+  isIOS: /iphone|ipad|ipod|itouch/gi.test(ua), // IOS
 
-           var prefix = opts.prefix || '__jp';
+  // JSONP请求数据
+  JSONP: (function () {
+    var count = 0;
+    function noop() { }
+    /**
+     * JSONP handler
+     *
+     * Options:
+     *  - param {String} qs parameter (`callback`)
+     *  - prefix {String} qs parameter (`__jp`)
+     *  - name {String} qs parameter (`prefix` + incr)
+     *  - timeout {Number} how long after a timeout error is emitted (`60000`)
+     *
+     * @param {String} url
+     * @param {Object|Function} optional options / callback
+     * @param {Function} optional callback
+     */
+    function jsonp(url, opts, fn) {
+      if ('function' == typeof opts) {
+        fn = opts;
+        opts = {};
+      }
+      if (!opts) opts = {};
 
-           // use the callback name that was passed if one was provided.
-           // otherwise generate a unique name by incrementing our counter.
-           var id = opts.name || (prefix + (count++));
+      var prefix = opts.prefix || '__jp';
 
-           var param = opts.param || 'callback';
-           var timeout = null != opts.timeout ? opts.timeout : 60000;
-           var enc = encodeURIComponent;
-           var target = document.getElementsByTagName('script')[0] || document.head;
-           var script;
-           var timer;
+      // use the callback name that was passed if one was provided.
+      // otherwise generate a unique name by incrementing our counter.
+      var id = opts.name || (prefix + (count++));
+
+      var param = opts.param || 'callback';
+      var timeout = null != opts.timeout ? opts.timeout : 60000;
+      var enc = encodeURIComponent;
+      var target = document.getElementsByTagName('script')[0] || document.head;
+      var script;
+      var timer;
 
 
-           if (timeout) {
-             timer = setTimeout(function(){
-               cleanup();
-               if (fn) fn(new Error('Timeout'));
-             }, timeout);
-           }
+      if (timeout) {
+        timer = setTimeout(function () {
+          cleanup();
+          if (fn) fn(new Error('Timeout'));
+        }, timeout);
+      }
 
-           function cleanup(){
-             if (script.parentNode) script.parentNode.removeChild(script);
-             window[id] = noop;
-             if (timer) clearTimeout(timer);
-           }
+      function cleanup() {
+        if (script.parentNode) script.parentNode.removeChild(script);
+        window[id] = noop;
+        if (timer) clearTimeout(timer);
+      }
 
-           function cancel(){
-             if (window[id]) {
-               cleanup();
-             }
-           }
-
-           window[id] = function(data){
-             // debug('jsonp got', data);
-             cleanup();
-             if (fn) fn(null, data);
-           };
-
-           // add qs component
-           url += (~url.indexOf('?') ? '&' : '?') + param + '=' + enc(id);
-           url = url.replace('?&', '?');
-
-           // debug('jsonp req "%s"', url);
-
-           // create script
-           script = document.createElement('script');
-           script.src = url;
-           target.parentNode.insertBefore(script, target);
-
-           return cancel;
+      function cancel() {
+        if (window[id]) {
+          cleanup();
         }
-        return jsonp;
-    })()
+      }
+
+      window[id] = function (data) {
+        // debug('jsonp got', data);
+        cleanup();
+        if (fn) fn(null, data);
+      };
+
+      // add qs component
+      url += (~url.indexOf('?') ? '&' : '?') + param + '=' + enc(id);
+      url = url.replace('?&', '?');
+
+      // debug('jsonp req "%s"', url);
+
+      // create script
+      script = document.createElement('script');
+      script.src = url;
+      target.parentNode.insertBefore(script, target);
+
+      return cancel;
+    }
+    return jsonp;
+  })()
 }
